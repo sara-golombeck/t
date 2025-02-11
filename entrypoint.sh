5dd12ecb40116f56288715abe7e1490f51136f07
sara_g <sara.beck.dev@gmail.com>
2025-02-11 09:46:45 +0200

    change jenkins file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,42 @@
+# #!/bin/sh
+
+# # Use TN_SIZE from environment variable, default to 150 if not set
+# SIZE=${TN_SIZE:-150}
+
+# # Process all image files in /pics directory
+# for file in /pics/*; do
+#     if [ -f "$file" ]; then
+#         echo "Processing: $file with size: $SIZE"
+#         ./thumbnail.sh "$file" "$SIZE"
+#     fi
+# done
 #!/bin/sh
+set -x  # לדיבוג מורחב
 
-# Use TN_SIZE from environment variable, default to 150 if not set
+# בדיקת הסביבה
+echo "Starting container with:"
+echo "User: $(id)"
+echo "Working directory: $(pwd)"
+echo "Contents of /pics:"
+ls -la /pics
+
+# הגדרת גודל
 SIZE=${TN_SIZE:-150}
+echo "Using thumbnail size: $SIZE"
 
-# Process all image files in /pics directory
+# עיבוד הקבצים
 for file in /pics/*; do
     if [ -f "$file" ]; then
         echo "Processing: $file with size: $SIZE"
         ./thumbnail.sh "$file" "$SIZE"
+        RESULT=$?
+        if [ $RESULT -ne 0 ]; then
+            echo "Error processing $file (exit code: $RESULT)"
+        else
+            echo "Successfully processed $file"
+        fi
     fi
 done
+
+echo "Final contents of /pics:"
+ls -la /pics
